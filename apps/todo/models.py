e814a2e3cbb396b08138930bbfbbcbe136eaf447
RoRep1ay <sojimmynea@gmail.com>
2024-08-07 10:27:30 +0700

    feat: add todos creator

--- apps/todo/models.py
+++ apps/todo/models.py
@@ -1,12 +1,18 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
 class TodoItem(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=256)
     completed = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+    )
 
     def __str__(self):
         return self.title
--- apps/todo/views.py
+++ apps/todo/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import TodoItem
 from django.contrib.auth.decorators import login_required
+# import user
+from django.contrib.auth.models import User
 
-@login_required
 def home(request):
-    todos = TodoItem.objects.all()
+    if request.user.is_authenticated:
+        todos = TodoItem.objects.filter(creator=request.user)
+    else:
+        todos = TodoItem.objects.filter(creator__isnull=True)
     return render(request, 'todo/index.html', {'items': todos})
 
 def create(request):
     if request.method == 'POST':
         title = request.POST.get('title')
-        todo = TodoItem.objects.create(title=title)
+        todo = TodoItem.objects.create(title=title, creator=request.user if request.user.is_authenticated else None)
         return render(request, 'todo/todo_item.html', {'it': todo})
     return HttpResponse(status=405, content='Method Not Allowed')
 
